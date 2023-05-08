f332b89d5cab4ed568059c732d8784f6ec457221
Sezer <goversuleyman@gmail.com>
2023-05-08 15:18:59 +0300

    API HAZIR

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using API.Core.DbModels;
 using API.Core.Interfaces;
 using API.Core.Specifications;
 using API.Dtos;
+using API.Helpers;
 using API.Infrastructure.DataContext;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -40,23 +41,18 @@ namespace API.Controllers
 
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts([FromQuery]ProductSpecParams productSpecParams)
         {
-            var spec = new  ProductsWithProductTypeAndBrandsSpecification();
-            var data = await _productRepository.ListAsync(spec);
-            //return Ok(data);
-           // return data.Select(pro => new ProductToReturnDto 
-           // {
-             //   Id = pro.Id,
-             //   Name = pro.Name,
-             //   Description = pro.Description,
-             //   PictureUrl = pro.PictureUrl,
-             //   Price = pro.Price,
-             //   ProductBrand = pro.ProductBrand != null ? pro.ProductBrand.Name : string.Empty,
-             //   ProductType = pro.ProductType != null ? pro.ProductType.Name : string.Empty
+            var spec = new  ProductsWithProductTypeAndBrandsSpecification(productSpecParams);
+            var countSpec = new ProductWithFiltersForCountSpecification(productSpecParams);
 
-            //}).ToList();
-            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(data));
+            var totalItems = await _productRepository.CountAsync(spec);
+
+            var products = await _productRepository.ListAsync(spec);
+
+            var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+            
+            return Ok(new Pagination<ProductToReturnDto>(productSpecParams.PageIndex, productSpecParams.PageSize, totalItems, data));
         }
 
 
